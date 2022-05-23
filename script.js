f64028cdae55d5b4ef123661f751c73af1fa960f
MatheusBaganha <matheusu.baganha@gmail.com>
2022-05-23 15:06:02 -0300

    responsividade e comentarios add

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const loader = document.querySelector('[data-loader]');
 const inputs = document.querySelectorAll('[data-input]');
 const btnEnviar = document.querySelector('[data-btn]');
 
+// ativa e desativa o loading
 function toggleLoad() {
   loader.classList.toggle('load');
 }
 
+//  Verifica se o CEP está formatado corretamente
 function validate(cep) {
   const matchCEP = cep.match(/[0-9]{5}[-. ]?[0-9]{3}/g);
   return (matchCEP && matchCEP[0] === cep);
 }
 
+//  Caso não esteja, será mostrado na tela inválido
 function notValid() {
   inputs.forEach((input) => {
     input.value = 'Cep Inválido';
@@ -23,6 +26,7 @@ function notValid() {
     cepInput.style.border = '2px solid red';
   });
 
+  //  Após 3s o aviso será limpado da tela
   setTimeout(() => {
     inputs.forEach((input) => {
       input.value = '';
@@ -42,12 +46,19 @@ async function updateInfo() {
     });
 
     let cepValue = cepInput.value;
+    //  Remove todos os - . espaços e não digitos do valor de cep
     cepValue = cepValue.replace(/[-. \D]/g, '');
+
+    //  Formata o cep digitado na tela
     cepInput.value = cepValue;
+
+    // Validação começa
     if (validate(cepValue)) {
       const cep = await fetch(`https://viacep.com.br/ws/${cepValue}/json/`);
       const cepJSON = await cep.json();
       toggleLoad();
+
+      //  Altera os endereços de acordo com o cep
       const logradouroInput = document.querySelector('[data-logradouro]');
       logradouroInput.value = cepJSON.logradouro;
 
@@ -61,6 +72,7 @@ async function updateInfo() {
       bairroInput.value = cepJSON.bairro;
     } else {
       notValid();
+      // Se não for um cep válido, ativa notValid
     }
   }
 
@@ -68,9 +80,11 @@ async function updateInfo() {
     notValid();
   }
 
+  // loader desativodo no final para evitar loading infinito em certas ocasiões
   loader.classList.remove('load');
 }
 
+//  previne que o formulário seja resetado quando clica no botão
 function btnPreventDefault(event) {
   event.preventDefault();
   this.addEventListener('click', updateInfo);
